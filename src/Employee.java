b1ef7dbcb9de3adf195b1d7678a2597e4b1782c8
Никита <nikit.orekhoff@yandex.ru>
2024-03-07 22:11:35 +0300

    Курсовая работа

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,8 @@
 public class Employee {
-    private String surname;
-    private String name;
-    private String patronymic;
-    private int department;
+    private final String surname;
+    private final String name;
+    private final String patronymic;
+    private static int department;
     private double salary;
     private static int idCounter = 0;
     private final int id;
@@ -40,10 +40,6 @@ public class Employee {
         return this.id;
     }
 
-    public static int getIdCounter() {
-        return idCounter;
-    }
-
     public void setDepartment(int department) {
         this.department = department;
     }
@@ -67,63 +63,5 @@ public class Employee {
     @Override
     public String toString() {
         return "Фамилия - " + surname + ", имя - " + name + ", отчество - " + patronymic + ", отдел № " + department + ", зарплата - " + salary + ", порядковый номер - " + id;
-    }
-
-    public static void getEmployeeBook(Employee[] employeeBook) {
-        for (int employee = 0; employee < employeeBook.length; employee++) {
-            System.out.println(employeeBook[employee].toString());
-        }
-    }
-
-    public static void getTotalSalary(Employee[] employeeBook) {
-        double totalSalary = 0;
-        for (Employee salary : employeeBook) {
-            totalSalary += salary.getSalary();
-        }
-        System.out.println("Общая сумма трат на зарплату составила " + totalSalary + " рублей.");
-    }
-
-    public static void getMaxSalary(Employee[] employeeBook) {
-        double maxSalary = Integer.MIN_VALUE;
-        String employeeMaxSalary = "";
-        for (Employee i : employeeBook) {
-            if (i.getSalary() > maxSalary)
-                maxSalary = i.getSalary();
-            if (maxSalary == i.getSalary()) {
-                employeeMaxSalary = i.getSurname() + " " + i.getName() + " " + i.getPatronymic();
-            }
-        }
-        System.out.println("Сотрудник - " + employeeMaxSalary + " с максимальной зарплатой - " + maxSalary + " рублей.");
-    }
-
-    public static void getMinSalary(Employee[] employeeBook) {
-        double minSalary = Integer.MAX_VALUE;
-        String employeeMinSalary = "";
-        for (Employee i : employeeBook) {
-            if (i.getSalary() <= minSalary)
-                minSalary = i.getSalary();
-            if (minSalary == i.getSalary()) {
-                employeeMinSalary = i.getSurname() + " " + i.getName() + " " + i.getPatronymic();
-            }
-        }
-        System.out.println("Сотрудник - " + employeeMinSalary + " с минимальной зарплатой - " + minSalary + " рублей.");
-    }
-
-    public static void getAverageSalary(Employee[] employeeBook) {
-        double totalSalary = 0;
-        double averageSalary = 0;
-        for (Employee salary : employeeBook) {
-            totalSalary += salary.getSalary();
-        }
-        averageSalary = totalSalary / employeeBook.length;
-        System.out.println("Средняя зарплата составила - " + averageSalary + " рублей.");
-    }
-
-    public static void getAllEmployee(Employee[] employeeBook) {
-        for (Employee employee : employeeBook) {
-            if (employee != null) {
-                System.out.println(employee.getSurname() + " " + employee.getName() + " " + employee.getPatronymic());
-            }
-        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,80 @@
 public class Main {
-    public static void main(String[] args) {
-        Employee[] employeeBook = new Employee[10];
-        employeeBook[0] = new Employee("Иванов", "Иван", "Иванович", 1, 35000);
-        employeeBook[1] = new Employee("Петров", "Петр", "Петрович", 2, 40000);
-        employeeBook[2] = new Employee("Сидоров", "Иван", "Петрович", 3, 45000);
-        employeeBook[3] = new Employee("Сидоров", "Петр", "Иванович", 4, 50000);
-        employeeBook[4] = new Employee("Смольков", "Артем", "Игоревич", 5, 55000);
-        employeeBook[5] = new Employee("Лебедев", "Игорь", "Андреевич", 1, 37500);
-        employeeBook[6] = new Employee("Малышев", "Владислав", "Альбертович", 2, 42500);
-        employeeBook[7] = new Employee("Белов", "Юрий", "Сергеевич", 3, 47500);
-        employeeBook[8] = new Employee("Чернов", "Сергей", "Юрьевич", 4, 52500);
-        employeeBook[9] = new Employee("Пятнов", "Евгений", "Евгеньевич", 5, 57500);
+    private static final Employee[] EMPLOYEE_BOOK = new Employee[10];
 
-        Employee.getEmployeeBook(employeeBook);
-        Employee.getTotalSalary(employeeBook);
-        Employee.getMaxSalary(employeeBook);
-        Employee.getMinSalary(employeeBook);
-        Employee.getAverageSalary(employeeBook);
-        Employee.getAllEmployee(employeeBook);
+
+    public static void printEmployeeBook() {
+        for (Employee employee : EMPLOYEE_BOOK) {
+            System.out.println(employee);
+        }
+    }
+
+    public static double getTotalSalary() {
+        double totalSalary = 0;
+        for (Employee salary : EMPLOYEE_BOOK) {
+            totalSalary += salary.getSalary();
+        }
+        return totalSalary;
+    }
+
+    public static Employee findMaxSalary() {
+        Employee employeeWithMaxSalary = null;
+        double maxSalary = Integer.MIN_VALUE;
+        for (Employee employee : EMPLOYEE_BOOK) {
+            if (employee.getSalary() > maxSalary) {
+                maxSalary = employee.getSalary();
+                employeeWithMaxSalary = employee;
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+
+    public static Employee findMinSalary() {
+        Employee employeeWithMinSalary = null;
+        double minSalary = Integer.MAX_VALUE;
+        for (Employee employee : EMPLOYEE_BOOK) {
+            if (employee.getSalary() <= minSalary) {
+                minSalary = employee.getSalary();
+                employeeWithMinSalary = employee;
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    public static double getAverageSalary() {
+        double totalSalary = 0;
+        double averageSalary = 0;
+        for (Employee salary : EMPLOYEE_BOOK) {
+            totalSalary += salary.getSalary();
+            averageSalary = totalSalary / EMPLOYEE_BOOK.length;
+        }
+        return averageSalary;
+    }
+
+    public static void printAllEmployee() {
+        for (Employee employee : EMPLOYEE_BOOK) {
+            if (employee != null) {
+            }
+            System.out.println(employee.getSurname() + " " + employee.getName() + " " + employee.getPatronymic());
+        }
+    }
+
+    public static void main(String[] args) {
+        EMPLOYEE_BOOK[0] = new Employee("Иванов", "Иван", "Иванович", 1, 35_000);
+        EMPLOYEE_BOOK[1] = new Employee("Петров", "Петр", "Петрович", 2, 40_000);
+        EMPLOYEE_BOOK[2] = new Employee("Сидоров", "Иван", "Петрович", 3, 45_000);
+        EMPLOYEE_BOOK[3] = new Employee("Сидоров", "Петр", "Иванович", 4, 50_000);
+        EMPLOYEE_BOOK[4] = new Employee("Смольков", "Артем", "Игоревич", 5, 55_000);
+        EMPLOYEE_BOOK[5] = new Employee("Лебедев", "Игорь", "Андреевич", 1, 37_500);
+        EMPLOYEE_BOOK[6] = new Employee("Малышев", "Владислав", "Альбертович", 2, 42_500);
+        EMPLOYEE_BOOK[7] = new Employee("Белов", "Юрий", "Сергеевич", 3, 47_500);
+        EMPLOYEE_BOOK[8] = new Employee("Чернов", "Сергей", "Юрьевич", 4, 52_500);
+        EMPLOYEE_BOOK[9] = new Employee("Пятнов", "Евгений", "Евгеньевич", 5, 57_500);
+
+        printEmployeeBook();
+        System.out.println("Общая сумма трат на зарплату составила " + getTotalSalary() + " рублей.");
+        System.out.println("Сотрудник с максимальной зарплатой: " + findMaxSalary());
+        System.out.println("Сотрудник с минимальной зарплатой: " + findMinSalary());
+        System.out.println("Средняя зарплата составила - " + getAverageSalary() + " рублей.");
+        printAllEmployee();
     }
 }
